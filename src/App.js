a330e41d06b171b6ea4c39f2123a062c98ac6766
= <=>
2023-04-07 20:06:12 +0000

    added a new file

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { Search } from "@mui/icons-material";
 
 
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = "http://codarnetwork.online/";
 axios.defaults.headers.post['Content-Type'] = "multipart/form-data";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
